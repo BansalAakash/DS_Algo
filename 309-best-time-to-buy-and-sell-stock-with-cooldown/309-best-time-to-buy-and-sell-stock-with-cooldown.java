8b55ddb825cbda55e4a6f8a21a19209e935e7147
Aakash Bansal <aakash6025@gmail.com>
2022-05-19 12:26:37 +0530

    Time: 4 ms (18.78%), Space: 42.4 MB (38.42%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,17 +1,44 @@
+class key{
+    public int index;
+    public boolean buy;
+    public int hashCode;
+    public key(int index, boolean buy){
+        this.index = index;
+        this.buy = buy;
+        this.hashCode = Objects.hash(index, buy);
+    }
+    @Override
+    public int hashCode() {
+        return this.hashCode;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        key that = (key) o;
+        return index == that.index && buy == that.buy;
+    }
+}
+
+
 class Solution {
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        if(n == 1) return 0;
-        int[] buy = new int[n];
-        int[] sell = new int[n];
-        buy[0] = -prices[0];
-        buy[1] = -Math.min(prices[0], prices[1]);
-        sell[0] = 0;
-        sell[1] = Math.max(0, buy[0] + prices[1]);
-        for(int i = 2;i < n;i++){
-            buy[i] = Math.max(sell[i - 2] - prices[i], buy[i - 1]);
-            sell[i] = Math.max(prices[i] + buy[i - 1], sell[i - 1]);
+    int profit (HashMap<key, Integer> cache, int[] prices, int index, boolean buying){
+        if(index >= prices.length){
+            return 0;
         }
-        return sell[n - 1];
+        key newKey = new key(index, buying);
+        if(cache.containsKey(newKey)) return cache.get(newKey);
+        if(buying){
+            cache.put(newKey, Math.max(profit(cache, prices, index + 1, buying), profit(cache, prices, index + 1, !buying) - prices[index]));
+        } else{
+            cache.put(newKey, Math.max(profit(cache, prices, index + 1, buying), profit(cache, prices, index + 2, !buying) + prices[index]));
+        }
+        return cache.get(newKey);
+    }
+    public int maxProfit(int[] prices) {
+        if(prices.length < 2) return 0;
+        return profit(new HashMap<>(), prices, 0, true);
     }
 }
