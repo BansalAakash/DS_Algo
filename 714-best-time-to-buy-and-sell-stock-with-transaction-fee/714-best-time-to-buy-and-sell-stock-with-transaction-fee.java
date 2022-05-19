2b536063a16a0c2455da230b117d69e1c9d4a2ee
Aakash Bansal <aakash6025@gmail.com>
2022-05-19 12:19:44 +0530

    Time: 434 ms (5.01%), Space: 221 MB (5.12%) - LeetHub

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,44 @@
+class key{
+    public int index;
+    public boolean buy;
+    public int hashCode;
+    public key(int index, boolean buy){
+        this.index = index;
+        this.buy = buy;
+        this.hashCode = Objects.hash(index, buy);
+    }
+    @Override
+    public int hashCode() {
+        return this.hashCode;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        key that = (key) o;
+        return index == that.index && buy == that.buy;
+    }
+}
+
+
+class Solution {
+    int profit (HashMap<key, Integer> cache, int[] prices, int fee, int index, boolean buying){
+        if(index == prices.length){
+            return 0;
+        }
+        key newKey = new key(index, buying);
+        if(cache.containsKey(newKey)) return cache.get(newKey);
+        if(buying){
+            cache.put(newKey, Math.max(profit(cache, prices, fee, index + 1, buying), profit(cache, prices, fee, index + 1, !buying) - prices[index]));
+        } else{
+            cache.put(newKey, Math.max(profit(cache, prices, fee, index + 1, buying), profit(cache, prices, fee, index + 1, !buying) + prices[index] - fee));
+        }
+        return cache.get(newKey);
+    }
+    public int maxProfit(int[] prices, int fee) {
+        if(prices.length < 2) return 0;
+        return profit(new HashMap<>(), prices, fee, 0, true);
+    }
+}
