9d2895e47980597d50ae581815080769f0a5b869
Aakash Bansal <aakash6025@gmail.com>
2022-05-23 11:51:36 +0530

    Time: 3 ms (58.90%), Space: 93.4 MB (20.80%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.java
+++ 896-monotonic-array/896-monotonic-array.java
@@ -2,12 +2,12 @@ class Solution {
     public boolean isMonotonic(int[] nums) {
         if(nums.length < 2) return true;
         int n = nums.length;
-        if(nums[0] >= nums[n - 1]){
-            for(int i = 1;i < nums.length;i++)
-                if(nums[i] > nums[i - 1]) return false;
-        } else{
-            for(int i = 1;i < nums.length;i++)
-                if(nums[i] < nums[i - 1]) return false;
+        int sign = 0;
+        for(int i = 1;i < nums.length;i++){
+            int curSign = Integer.compare(nums[i], nums[i - 1]);
+            if(sign != 0 && curSign != 0 && curSign != sign) return false;
+            if(curSign != 0)
+                sign = curSign;
         }
         return true;
     }
