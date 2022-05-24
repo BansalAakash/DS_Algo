7c91d3dc938ab12d6d9aea63a067b197d246a9be
Aakash Bansal <aakash6025@gmail.com>
2022-05-24 08:53:40 +0530

    Time: 10 ms (15.08%), Space: 47.7 MB (41.00%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -5,10 +5,7 @@ class Solution {
         for(int i = 1;i < m;i++){
             min = matrix[i][0] + matrix[i - 1][0];
             for(int j = 0;j < m;j++){
-                int temp = matrix[i - 1][j];
-                if(j > 0) temp = Math.min(temp, matrix[i - 1][j - 1]);
-                if(j < m - 1) temp = Math.min(temp, matrix[i - 1][j + 1]);
-                matrix[i][j] = temp + matrix[i][j];
+                matrix[i][j] += Math.min(matrix[i - 1][j], Math.min(matrix[i - 1][Math.max(0, j - 1)], matrix[i - 1][Math.min(m - 1, j + 1)]));
                 min = Math.min(min, matrix[i][j]);
             }
         }
