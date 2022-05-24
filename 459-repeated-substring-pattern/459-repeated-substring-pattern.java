bc166fa08c14f54e21d7f844a6e3735939cec496
Aakash Bansal <aakash6025@gmail.com>
2022-05-24 10:06:15 +0530

    Time: 846 ms (7.71%), Space: 447.5 MB (5.01%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         int size = s.length();
-        for(int i = 1;i <= size / 2;i++){
+        for(int i = size / 2;i >= 1;i--){
             if(size % i == 0){
                 String result = "", sub = s.substring(0, i);
                 int j = size / i;
