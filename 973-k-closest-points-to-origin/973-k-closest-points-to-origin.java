0071cec6eb39d24850f99f12acf6256a180aa7e0
Aakash Bansal <aakash6025@gmail.com>
2022-05-30 09:51:35 +0530

    Time: 33 ms (75.60%), Space: 119.6 MB (5.38%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -0,0 +1,27 @@
+class Point{
+    int x;
+    int y;
+    double d;
+    public Point(int a, int b){
+        x = a;
+        y = b;
+        d = Math.sqrt(x * x + y * y); //distance to the origin
+    }
+}
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Point> q = new PriorityQueue<>((a, b)-> (a.d < b.d) ? -1 : 1);
+        for(int[] point : points)
+            q.add(new Point(point[0], point[1]));
+        int[][] result = new int[k][2];
+        int i = 0;
+        while(i < k){
+            Point p = q.poll();
+            result[i][0] = p.x;
+            result[i][1] = p.y;
+            i++;
+        }
+        return result;
+    }
+}
