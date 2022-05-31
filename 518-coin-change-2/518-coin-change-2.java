278b4a343e92cceda90ad54751670eda5c2cc235
Aakash Bansal <aakash6025@gmail.com>
2022-05-31 10:01:03 +0530

    Time: 6 ms (58.25%), Space: 42.1 MB (59.50%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,17 +1,11 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        Arrays.sort(coins);
-        dp[0][0] = 1;                       //1 way of making amount 0 without coins
-        for(int i = 1;i <= n;i++){
-            dp[i][0] = 1;                   //There is always 1 way of making amount 0 with any coins
-            for(int j = 1;j <= amount;j++){
-                dp[i][j] = dp[i - 1][j];
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;                   //There is always 1 way of making amount 0 with any coins
+        for(int i = 1;i <= coins.length;i++)
+            for(int j = 1;j <= amount;j++)
                 if(j >= coins[i - 1])
-                    dp[i][j] += dp[i][j - coins[i - 1]];        //take the coin
-            }
-        }
-        return dp[n][amount];
+                    dp[j] += dp[j - coins[i - 1]];
+        return dp[amount];
     }
 }
