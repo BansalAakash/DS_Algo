58e412f23f8e50479b17e35994fc3a0ff3a4c21c
Aakash Bansal <aakash6025@gmail.com>
2022-05-31 09:04:24 +0530

    Time: 32 ms (40.90%), Space: 44.9 MB (57.76%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,22 +1,19 @@
 class Solution {
-    int helper(int[] coins, int amount, int[] dp) {
-        if(amount == 0) return 0;
-        if(amount < 0) return -1;
-        if(dp[amount] != 0) return dp[amount];
-        int min_ = Integer.MAX_VALUE;
-        for(int i : coins){
-            int temp = Math.min(min_, helper(coins, amount - i, dp));
-            if(temp != -1)
-                min_ = Math.min(min_, temp);
-        }
-        if(min_ == Integer.MAX_VALUE) return dp[amount] = -1;
-        else dp[amount] = 1 + min_;
-        return dp[amount];
-    }
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
-        // Arrays.sort(coins);
-        int result = helper(coins, amount, dp);
-        return result;
+        int n = coins.length;
+        for(int i = 1;i <= amount;i++){
+            for(int j = 0;j < n;j++){
+                if(i >= coins[j] && dp[i - coins[j]] != -1){
+                    if(dp[i] == 0) dp[i] = 1 + dp[i - coins[j]];
+                    else dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
+            if(dp[i] == 0) dp[i] = -1;
+        }
+        return dp[amount];
     }
 }
+
+
+//Doing this "-1" stuff because we have to return -1 if no combination found
