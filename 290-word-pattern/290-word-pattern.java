1d2c2aef0051f78993ceabbc4290d5d057bd828d
Aakash Bansal <aakash6025@gmail.com>
2022-06-08 08:32:37 +0530

    Time: 1 ms (94.39%), Space: 42.5 MB (8.09%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] str = s.split(" ");
+        if(str.length != pattern.length())
+            return false;
+        HashMap<Character, String> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        for(int i = 0;i < pattern.length();i++){
+            char ch = pattern.charAt(i);
+            if(map.containsKey(ch) && !map.get(ch).equals(str[i]))
+                return false;
+            if(!map.containsKey(ch) && set.contains(str[i]))
+                return false;
+            map.putIfAbsent(ch, str[i]);
+            set.add(str[i]);
+        }
+        return true;
+    }
+}
