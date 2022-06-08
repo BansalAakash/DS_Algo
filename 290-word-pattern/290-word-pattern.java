eae1afe8ab05e3309434c8d7dd89c9ba688b964d
Aakash Bansal <aakash6025@gmail.com>
2022-06-08 08:38:48 +0530

    Time: 1 ms (94.39%), Space: 40.4 MB (75.13%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -4,15 +4,15 @@ class Solution {
         if(str.length != pattern.length())
             return false;
         HashMap<Character, String> map = new HashMap<>();
-        HashSet<String> set = new HashSet<>();
+        // HashSet<String> set = new HashSet<>();
         for(int i = 0;i < pattern.length();i++){
             char ch = pattern.charAt(i);
             if(map.containsKey(ch) && !map.get(ch).equals(str[i]))
                 return false;
-            if(!map.containsKey(ch) && set.contains(str[i]))
+            if(!map.containsKey(ch) && map.containsValue(str[i]))
                 return false;
             map.putIfAbsent(ch, str[i]);
-            set.add(str[i]);
+            // set.add(str[i]);
         }
         return true;
     }
