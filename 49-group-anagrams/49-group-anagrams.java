ed2a202a353e6b6d0dac6dc87a65135b1d640839
Aakash Bansal <aakash6025@gmail.com>
2022-06-09 10:28:55 +0530

    Time: 21 ms (30.36%), Space: 46.6 MB (76.68%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,14 +1,14 @@
 class Solution {
-    int getHash(String str){
+    String getHash(String str){
         int[] freq = new int[26];
         for(char ch : str.toCharArray())
             freq[ch - 'a']++;
-        return Arrays.hashCode(freq);
+        return Arrays.toString(freq);
     }
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<Integer, List<String>> map = new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
         for(String str : strs){
-            int hash = getHash(str);
+            String hash = getHash(str);
             if(map.containsKey(hash))
                  map.get(hash).add(str);
             else{
