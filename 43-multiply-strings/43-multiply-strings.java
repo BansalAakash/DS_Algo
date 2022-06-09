5dd301849de8c3d769f424f11f2481b0571e2007
Aakash Bansal <aakash6025@gmail.com>
2022-06-09 11:05:52 +0530

    Time: 58 ms (9.73%), Space: 61.1 MB (6.31%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,46 +1,51 @@
 class Solution {
-    ArrayList<Integer> multiply(ArrayList<Integer> list, int num){
-        ArrayList<Integer> result = new ArrayList<>();
-        int n = list.size(), carry = 0;
-        for(int i = n - 1;i >= 0;i--){
-            int temp = list.get(i) * num + carry;
-            result.add(0, temp % 10);
-            carry = temp / 10;
+    StringBuilder add(StringBuilder str1, StringBuilder str2){
+        int m = str1.length() - 1, n = str2.length() - 1, carry = 0;
+        StringBuilder str = new StringBuilder();
+        while(m >= 0 || n >= 0){
+            int val1 = m >= 0 ? str1.charAt(m) - '0' : 0;
+            int val2 = n >= 0 ? str2.charAt(n) - '0' : 0;
+            int sum = val1 + val2 + carry;
+            str.insert(0, sum % 10);
+            carry = sum / 10;
+            m--;n--;
         }
-        if(carry > 0) result.add(0, carry);
-        return result;
+        if(carry > 0)
+            str.insert(0, carry);
+        return str;
     }
-    ArrayList<Integer> add(ArrayList<Integer> list1, ArrayList<Integer> list2){
-        int m = list1.size(), n = list2.size(), carry = 0;
-        ArrayList<Integer> result = new ArrayList<>();
-        for(int i = 0;i < Math.max(m, n);i++){
-            int val1 = i < m ? list1.get(m - i - 1) : 0;
-            int val2 = i < n ? list2.get(n - i - 1) : 0;
-            int temp = val1 + val2 + carry;
-            result.add(0, temp % 10);
-            carry = temp / 10;
+    StringBuilder multiplySingle(String s, char ch){
+        int m = s.length() - 1, carry = 0, val = ch - '0';
+        StringBuilder str = new StringBuilder();
+        // System.out.println(s + ", " + val);
+        while(m >= 0){
+            int sum = (s.charAt(m) - '0') * val + carry;
+            carry = sum / 10;
+            sum = sum % 10;
+            str.insert(0, sum);
+            m--;
         }
-        if(carry > 0) result.add(0, carry);
-        return result;
+        if(carry > 0)
+            str.insert(0, carry);
+        // System.out.println(str.toString());
+        return str;
     }
     public String multiply(String num1, String num2) {
-        if(num1.equals("0") || num2.equals("0")) return "0";
-        int m = num1.length(), n = num2.length();
+        int m = num1.length() - 1, n = num2.length() - 1;
         if(m < n) return multiply(num2, num1);
-        ArrayList<Integer> num1_list = new ArrayList<>();
-        ArrayList<Integer> result = new ArrayList<>();
-        for(char ch : num1.toCharArray()) num1_list.add(ch - '0');
         int shift = 0;
-        for(int i = n - 1;i >= 0;i--){
-            ArrayList<Integer> temp = multiply(num1_list, num2.charAt(i) - '0');
+        StringBuilder result = new StringBuilder();
+        for(int i = n;i >= 0;i--){
+            StringBuilder temp = multiplySingle(num1, num2.charAt(i));
             for(int j = 0;j < shift;j++)
-                temp.add(0);
+                temp.append('0');
             result = add(result, temp);
             shift++;
         }
-        StringBuilder builder = new StringBuilder();
-        for(int i : result)
-            builder.append(i);
-        return builder.toString();
+        int i = 0;
+        while(result.length() > 0 && result.charAt(0) == '0')
+            result.deleteCharAt(0);
+        String output = result.toString();
+        return output.length() > 0 ? output : "0";
     }
 }
