455edd7f713e1f98382c4c9b9bb6eef1792df5c2
Aakash Bansal <aakash6025@gmail.com>
2022-06-10 09:57:11 +0530

    Time: 48 ms (91.25%), Space: 62.5 MB (19.83%) - LeetHub

--- 1797-design-authentication-manager/1797-design-authentication-manager.java
+++ 1797-design-authentication-manager/1797-design-authentication-manager.java
@@ -1,26 +1,41 @@
 class AuthenticationManager {
     HashMap<String, Integer> map;
+    TreeMap<Integer, String> time_map;
     int ttl;
     public AuthenticationManager(int timeToLive) {
         map = new HashMap<String, Integer>();
+        time_map = new TreeMap<>();
         ttl = timeToLive;
     }
     public void generate(String tokenId, int currentTime) {
+        clean(currentTime);
         map.put(tokenId, currentTime + ttl);
+        time_map.put(currentTime + ttl, tokenId);
     }
     
     public void renew(String tokenId, int currentTime) {
-        if(map.containsKey(tokenId) && map.get(tokenId) > currentTime)
+        clean(currentTime);
+        if(map.containsKey(tokenId)){
+            time_map.remove(map.get(tokenId));
+            time_map.put(currentTime + ttl, tokenId);
             map.put(tokenId, currentTime + ttl);
+        }
     }
     
     public int countUnexpiredTokens(int currentTime) {
-        int count = 0;
-        for(Map.Entry<String, Integer> entry : map.entrySet()){
-            if(entry.getValue() > currentTime)
-                count++;
+        clean(currentTime);
+        return map.size();
+    }
+    
+    void clean(int currentTime){
+        Iterator<Map.Entry<Integer, String>> it = time_map.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry<Integer, String> entry = it.next();
+            if(entry.getKey() > currentTime)
+                break;
+            map.remove(entry.getValue());
+            it.remove();
         }
-        return count;
     }
 }
 
