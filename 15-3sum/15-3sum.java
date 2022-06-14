86e4eb42019b93e8ca9d9c24dd37637a3df8da90
Aakash Bansal <aakash6025@gmail.com>
2022-06-14 11:19:45 +0530

    Time: 35 ms (48.86%), Space: 46.3 MB (94.86%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,26 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
+        int cur = 0, n = nums.length;
         List<List<Integer>> result = new ArrayList<>();
         if(n < 3) return result;
         Arrays.sort(nums);
-        for(int i = 0;i < n - 2;i++){
-            if(i == 0 || nums[i] != nums[i - 1]){
-                int start = i + 1, end = n - 1, sum = 0 - nums[i];
-                while(start < end){
-                    if(nums[start] + nums[end] == sum){
-                        result.add(Arrays.asList(nums[i], nums[start], nums[end]));
-                        while(start < n - 1 && nums[start + 1] == nums[start]) start++;
-                        start++;
-                    } else if(nums[start] + nums[end] > sum){
-                        while(end > i && nums[end - 1] == nums[end]) end--;
-                        end--;
-                    }else{
-                        while(start < n - 1 && nums[start + 1] == nums[start]) start++;
-                        start++;
-                    }
+        while(cur < n - 2){
+            int i = cur + 1, j = n - 1;
+            while(i < j){
+                int sum = nums[cur] + nums[i] + nums[j];
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[cur], nums[i], nums[j]));
+                    while(i < j && nums[j - 1] == nums[j])j--;
+                    j--;
+                    while(i < j && nums[i + 1] == nums[i])i++;
+                    i++;
+                }
+                else if(sum > 0) {
+                    while(i < j && nums[j - 1] == nums[j])j--;
+                    j--;
+                } else{
+                    while(i < j && nums[i + 1] == nums[i])i++;
+                    i++;
                 }
             }
+            while(cur < n - 2 && nums[cur + 1] == nums[cur]) cur++;
+            cur++;
         }
         return result;
     }
