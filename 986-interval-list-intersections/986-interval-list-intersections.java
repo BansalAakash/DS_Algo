bfa131d24f55f394d2c1308789e834dade25979d
Aakash Bansal <aakash6025@gmail.com>
2022-06-15 23:43:59 +0530

    Time: 6 ms (32.07%), Space: 54.5 MB (56.34%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.java
+++ 986-interval-list-intersections/986-interval-list-intersections.java
@@ -3,20 +3,13 @@ class Solution {
         int i = 0, j = 0, m = A.length, n = B.length;
         List<int[]> list = new ArrayList<>();
         while(i < m && j < n){
-            if((B[j][0] <= A[i][1] && A[i][0] <= B[j][1]) || (A[i][0] <= B[j][1] && B[j][0] <= A[i][1]))       //overlap
+            if(Math.max(A[i][0], B[j][0]) <= Math.min(A[i][1], B[j][1]))       //overlap
                 list.add(new int[] {Math.max(A[i][0], B[j][0]), Math.min(A[i][1], B[j][1])});
             if(A[i][1] < B[j][1])                           //A[i] ends first
                 i++;
-            else if(A[i][1] > B[j][1])                      //B[j] ends first
+            else
                 j++;
-            else {                                          //Both end at same time
-                i++;
-                j++;
-            }
         }
-        int[][] result = new int[list.size()][2];
-        for(i = 0;i < list.size();i++)
-            result[i] = list.get(i);
-        return result;
+        return list.toArray(new int[list.size()][]);
     }
 }
