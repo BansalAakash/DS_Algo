9f57559346de8db2bb240c0e80fa74fa1a6aa289
Aakash Bansal <aakash6025@gmail.com>
2022-06-16 23:59:31 +0530

    Time: 14 ms (63.66%), Space: 48.3 MB (38.12%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,25 +1,21 @@
 class Solution {
-    boolean isAnagram(int[] freq1, int[] freq2){
-        for(int i = 0;i < 26;i++)
-            if(freq1[i] != freq2[i]) return false;
-        return true;
-    }
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
-        int p_n = p.length(), s_n = s.length();
-        if(s_n < p_n) return result;
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-        for(int i = 0;i < p.length();i++){
-            freq1[p.charAt(i) - 'a']++;
-            freq2[s.charAt(i) - 'a']++;
+        int m = s.length(), n = p.length();
+        if(m < n)
+            return result;
+        int[] p_ = new int[26];
+        int[] s_ = new int[26];
+        for(int i = 0;i < n - 1;i++){
+            p_[p.charAt(i) - 'a']++;
+            s_[s.charAt(i) - 'a']++;
         }
-        if(isAnagram(freq1, freq2))
-            result.add(0);
-        for(int i = p_n;i < s_n;i++){
-            freq2[s.charAt(i - p_n) - 'a']--;
-            freq2[s.charAt(i) - 'a']++;
-            if(isAnagram(freq1, freq2)) result.add(i - p_n + 1);
+        p_[p.charAt(n - 1) - 'a']++;
+        for(int i = 0;i <= m - n;i++){
+            s_[s.charAt(i + n - 1) - 'a']++;
+            if(Arrays.equals(p_, s_))
+                result.add(i);
+            s_[s.charAt(i) - 'a']--;
         }
         return result;
     }
