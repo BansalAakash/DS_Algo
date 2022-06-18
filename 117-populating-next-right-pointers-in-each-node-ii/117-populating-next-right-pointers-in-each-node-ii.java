0a8cb9d9d4a594bf0e1c4e200718fb2a9b37b1ab
Aakash Bansal <aakash6025@gmail.com>
2022-06-18 23:23:55 +0530

    Time: 1 ms (74.51%), Space: 44.6 MB (64.88%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public Node connect(Node root) {
+        Node dummy = new Node(-1), p = dummy, head = root;
+        while(head != null){
+            Node cur = head, pre = null, nextHead = null;
+            while(cur != null){
+                if(cur.left != null){
+                    if(pre == null) {
+                        pre = cur.left;
+                        nextHead = pre;
+                    }
+                    else{
+                        pre.next = cur.left;
+                        pre = pre.next;
+                    }
+                }
+                if(cur.right != null){
+                    if(pre == null) {
+                        pre = cur.right;
+                        nextHead = pre;
+                    }
+                    else{
+                        pre.next = cur.right;
+                        pre = pre.next;
+                    }
+                }
+                cur = cur.next;
+            }
+            head = nextHead;
+        }
+        return root;
+    }
+}
