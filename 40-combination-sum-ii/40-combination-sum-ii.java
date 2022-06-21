164fe30af6fd5f6ad6781cfa68602a4729f5d2b5
Aakash Bansal <aakash6025@gmail.com>
2022-06-21 10:37:02 +0530

    Time: 10 ms (21.89%), Space: 44.3 MB (22.59%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    void solve(int[] nums, List<List<Integer>> result, List<Integer> list, HashSet<Integer> taken, int sum, int target, int start){
+    void solve(int[] nums, List<List<Integer>> result, List<Integer> list, int sum, int target, int start){
         if(sum > target)
             return;
         if(sum == target){
@@ -7,19 +7,17 @@ class Solution {
             return;
         }
         for(int i = start;i < nums.length;i++){
-            if(taken.contains(i) || (i > start && nums[i] == nums[i - 1]))
+            if(i > start && nums[i] == nums[i - 1])
                 continue;
             list.add(nums[i]);
-            taken.add(i);
-            solve(nums, result, list, taken, sum + nums[i], target, i + 1);
+            solve(nums, result, list, sum + nums[i], target, i + 1);
             list.remove(list.size() - 1);
-            taken.remove(i);
         }
     }
     public List<List<Integer>> combinationSum2(int[] nums, int target) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        solve(nums, result, new ArrayList<>(), new HashSet<>(), 0, target, 0);
+        solve(nums, result, new ArrayList<>(), 0, target, 0);
         return result;
     }
 }
