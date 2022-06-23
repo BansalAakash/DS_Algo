5bf2569209eecbe4217e631315af3e3e2847c318
Aakash Bansal <aakash6025@gmail.com>
2022-06-23 10:47:19 +0530

    Time: 315 ms (15.40%), Space: 117.3 MB (16.95%) - LeetHub

---
+++ 79-word-search/79-word-search.java
@@ -0,0 +1,27 @@
+class Solution {
+    boolean solve(char[][] board, int i, int j, int m, int n, String word, int pos, boolean[][] visited){
+        if(pos == word.length())
+            return true;
+        if(i < 0 || j < 0 || i == m || j == n || visited[i][j] || board[i][j] != word.charAt(pos))
+            return false;
+        visited[i][j] = true;
+        int[] dirs = {-1, 0, 1, 0, -1};
+        for(int k = 0;k < 4;k++)
+            if(solve(board, i + dirs[k], j + dirs[k + 1], m, n, word, pos + 1, visited))
+                return true;
+        visited[i][j] = false;
+        return false;
+    }
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+        boolean result = false;
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                result = result || solve(board, i, j, m, n, word, 0, new boolean[m][n]);
+                if(result == true)
+                    return result;
+            }
+        }
+        return result;
+    }
+}
