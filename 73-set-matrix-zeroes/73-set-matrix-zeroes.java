54ed76360679814fa01e4657262230856824db52
Aakash Bansal <aakash6025@gmail.com>
2022-09-05 13:34:42 +0530

    Time: 1 ms (97.67%), Space: 53.8 MB (71.61%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,19 +1,31 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        HashSet<Integer> colSet = new HashSet<>();
-        HashSet<Integer> rowSet = new HashSet<>();
+        int firstColMarked = 1;
         for(int i = 0;i < matrix.length;i++){
             for(int j = 0;j < matrix[0].length;j++){
                 if(matrix[i][j] == 0){
-                    rowSet.add(i);
-                    colSet.add(j);
+                    matrix[i][0] = 0;
+                    if(j == 0)
+                        firstColMarked = 0;
+                    else
+                        matrix[0][j] = 0;
                 }
             }
         }
-        for(int i = 0;i < matrix.length;i++){
-            for(int j = 0;j < matrix[0].length;j++){
-                if(rowSet.contains(i) || colSet.contains(j))
+        for(int i = 1;i < matrix.length;i++){
+            for(int j = 1;j < matrix[0].length;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
                     matrix[i][j] = 0;
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int j = 0;j < matrix[0].length;j++){
+                matrix[0][j] = 0;           //set first row as 0
+            }
+        }
+        if(firstColMarked == 0){
+            for(int i = 0;i < matrix.length;i++){
+                matrix[i][0] = 0;           //set first col as 0
             }
         }
     }
