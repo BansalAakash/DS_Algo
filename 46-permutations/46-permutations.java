5d89f9aa2a45379b225cda79d51c8b5ab9b96fa9
Aakash Bansal <aakash6025@gmail.com>
2022-09-05 14:52:44 +0530

    Time: 1 ms (98.37%), Space: 42.7 MB (87.91%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,22 +1,22 @@
 class Solution {
-    void solve(List<List<Integer>> result, List<Integer> list, HashSet<Integer> taken, int[] nums){
-        if(taken.size() == nums.length){
-            result.add(new ArrayList<>(list));
-            return;
-        }
-        for(int i = 0;i < nums.length;i++){
-            if(!taken.contains(i)){
-                list.add(nums[i]);
-                taken.add(i);
-                solve(result, list, taken, nums);
-                list.remove(list.size() - 1);
-                taken.remove(i);
-            }
-        }
-    }
+    void permute(List<List<Integer>> result, ArrayList<Integer> cur, HashSet<Integer> set, int[] nums){
+		if(set.size() == nums.length){
+			result.add(new ArrayList<>(cur));
+			return;
+		}
+		for(int i = 0;i < nums.length;i++){
+			if(!set.contains(i)){
+				cur.add(nums[i]);
+				set.add(i);
+				permute(result, cur, set, nums);
+				cur.remove(cur.size() - 1);
+				set.remove(i);
+			}
+		}
+	}
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        solve(result, new ArrayList<>(), new HashSet<>(), nums);
-        return result;
+		permute(result, new ArrayList<>(), new HashSet<>(), nums);
+		return result;
     }
 }
