7c549689a427c6872a3c6326177e5b939053ee4e
Aakash Bansal <aakash6025@gmail.com>
2022-09-07 17:00:36 +0530

    Time: 6 ms (43.99%), Space: 56.8 MB (20.18%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,11 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length;
-        for(int i = 0;i <= (n - 1) / 2;i++){
-            if(nums[i + n / 2] == nums[i])
-                return nums[i];
-        }
-        return -1;
+        return nums[nums.length / 2];
     }
 }
