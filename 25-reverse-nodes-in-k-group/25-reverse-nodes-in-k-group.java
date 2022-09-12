9aaea3493d66e809d07e674c23e04135b6c24c7e
Aakash Bansal <aakash6025@gmail.com>
2022-09-12 10:39:17 +0530

    Time: 1 ms (62.97%), Space: 45 MB (81.81%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -9,36 +9,32 @@
  * }
  */
 class Solution {
-    int length(ListNode head){
-        int len = 0;
-        while(head != null){
-            head = head.next;
-            len++;
+    private ListNode helper(int rem, int k, ListNode head){
+        if(rem < k)
+            return head;
+        ListNode cur = head, prev = null, next = null;
+        int temp = 0;
+        while(temp++ < k){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
         }
-        return len;
+        head.next = helper(rem - k, k, cur);
+        return prev;
+    }
+    private int length(ListNode head){
+        int l = 0;
+        while(head != null){
+            l++;
+            head = head.next;
+        }
+        return l;
     }
     public ListNode reverseKGroup(ListNode head, int k) {
-        int size = length(head);
-        if(size < k) return head;
-        ListNode cur = head, prev = null, mainHead = null, prevTail = null, newHead = null;
-        while(size >= k){
-            int count = k, tempC = k;
-            ListNode temp1 = cur;
-            newHead = cur;
-            while(tempC-- > 0)
-                temp1 = temp1.next;
-            prev = temp1;
-            while(count-- > 0){
-                ListNode temp = cur.next;
-                cur.next = prev;
-                prev = cur;
-                cur = temp;
-            }
-            if(mainHead == null) mainHead = prev;
-            if(prevTail != null) prevTail.next = prev;
-            prevTail = newHead;
-            size -= k;
-        }
-        return mainHead;
+        if(k == 1 || head.next == null)
+            return head;
+        head = helper(length(head), k, head);
+        return head;
     }
 }
