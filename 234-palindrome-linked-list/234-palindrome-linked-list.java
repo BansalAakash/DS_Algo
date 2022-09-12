8ef9bea1a2dccd3e39f2991f97339d7bc7eb8f62
Aakash Bansal <aakash6025@gmail.com>
2022-09-12 15:54:13 +0530

    Time: 9 ms (52.22%), Space: 97.3 MB (66.35%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -9,23 +9,41 @@
  * }
  */
 class Solution {
+    private ListNode reverse(ListNode head){
+        if(head == null || head.next == null)
+            return head;
+        ListNode cur = head, next = null, prev = null;
+        while(cur != null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+    private void display(ListNode head){
+        while(head != null){
+            System.out.print(head.val + ", ");
+            head = head.next;
+        }
+        System.out.println();
+    }
     public boolean isPalindrome(ListNode head) {
         if(head == null || head.next == null)
             return true;
-        Stack<Integer> stack = new Stack<>();
-        ListNode slow = head, fast = head;
+        ListNode slow = head, fast = head.next;
         while(fast != null && fast.next != null){
-            stack.add(slow.val);
             slow = slow.next;
             fast = fast.next.next;
         }
-        if(fast != null)
-            slow = slow.next;
-        while(!stack.isEmpty()){
-            if(stack.pop() != slow.val)
-                return false;
-            slow = slow.next;
+        slow.next = reverse(slow.next);
+        ListNode temp1 = head, temp2 = slow.next;
+        while(temp2 != null && temp1.val == temp2.val){
+            temp1 = temp1.next;
+            temp2 = temp2.next;
         }
-        return true;
+        slow.next = reverse(slow.next);
+        // display(head);
+        return temp2 == null;
     }
 }
