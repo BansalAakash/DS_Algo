4337087f0ac84c2f48c7dbc77a938247776645df
Aakash Bansal <aakash6025@gmail.com>
2022-09-13 13:47:55 +0530

    Time: 25 ms (91.37%), Space: 46.6 MB (91.05%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,31 +1,27 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int cur = 0, n = nums.length;
-        List<List<Integer>> result = new ArrayList<>();
-        if(n < 3) return result;
         Arrays.sort(nums);
-        while(cur < n - 2){
-            int i = cur + 1, j = n - 1;
-            while(i < j){
-                int sum = nums[cur] + nums[i] + nums[j];
-                if(sum == 0){
-                    result.add(Arrays.asList(nums[cur], nums[i], nums[j]));
-                    while(i < j && nums[j - 1] == nums[j])j--;
-                    j--;
-                    while(i < j && nums[i + 1] == nums[i])i++;
-                    i++;
+        int n = nums.length, i = 0, j = 1, k = n - 1;
+        List<List<Integer>> result = new ArrayList<>();
+        if(n < 3)
+            return result;
+        while(i < n - 2){
+            j = i + 1;
+            k = n - 1;
+            while(j < k){
+                if(nums[j] + nums[k] == -nums[i]){
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while(j < k && nums[j + 1] == nums[j]) j++;
+                    while(j < k && nums[k - 1] == nums[k]) k--;
+                    j++;
+                    k--;
                 }
-                else if(sum > 0) {
-                    while(i < j && nums[j - 1] == nums[j])j--;
-                    j--;
-                } else{
-                    while(i < j && nums[i + 1] == nums[i])i++;
-                    i++;
-                }
+                else if(nums[j] + nums[k] > -nums[i])
+                    k--;
+                else j++;
             }
-            while(cur < n - 2 && nums[cur + 1] == nums[cur]) cur++;
-            cur++;
-        }
-        return result;
+            while(i < n - 2 && nums[i + 1] == nums[i]) i++;
+            i++;
+        }return result;
     }
 }
