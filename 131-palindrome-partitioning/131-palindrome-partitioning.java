b0d7170eae9bebc60fefb075eca768b0740c7e16
Aakash Bansal <aakash6025@gmail.com>
2022-09-16 12:59:48 +0530

    Time: 18 ms (64.29%), Space: 185.5 MB (63.86%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,38 +1,22 @@
 class Solution {
-    boolean isPal(String s, int i, int j){ // index stores index of partition
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j))
-                return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    void helper(List<List<String>> result, ArrayList<Integer> partitions, String s, int index){
-        if(index == s.length()){
-            if(partitions.size() > 0){
-                int start = 0, end = -1;
-                ArrayList<String> list = new ArrayList<>();
-                for(int i = 0;i < partitions.size();i++){
-                    end = partitions.get(i);
-                    list.add(s.substring(start, end));
-                    start = end;
-                }
-                result.add(list);
-            }
+    void helper(List<List<String>> result, ArrayList<String> list, String s, int start, boolean[][] dp){
+        if(start == s.length()){
+            result.add(new ArrayList<>(list));
             return;
         }
-        for(int i = 1; i + index <= s.length();i++){
-            if(isPal(s, index, index + i - 1)){
-                partitions.add(index + i);
-                helper(result, partitions, s, index + i);
-                partitions.remove(partitions.size() - 1);
+        for(int end = start; end < s.length();end++){
+            if(s.charAt(start) == s.charAt(end) && ((end - start) <= 2 || dp[start + 1][end - 1])){
+                dp[start][end] = true;
+                list.add(s.substring(start, end + 1));
+                helper(result, list, s, end + 1, dp);
+                list.remove(list.size() - 1);
             }
         }
     }
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
-        helper(result, new ArrayList<Integer>(), s, 0);
+        int n = s.length();
+        helper(result, new ArrayList<String>(), s, 0, new boolean[n + 1][n + 1]);
         return result;
     }
 }
