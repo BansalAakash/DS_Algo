3722eb14df76be83e2934b9d23210ce647f657f2
Aakash Bansal <aakash6025@gmail.com>
2022-09-16 10:15:38 +0530

    Time: 3 ms (49.30%), Space: 44.1 MB (59.18%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,23 +1,27 @@
 class Solution {
-    void solve(List<List<Integer>> result, List<Integer> list, int[] nums, int n, int i, boolean c){
-        //c keeps track of whether we chose prev or not, c doesn't matter for i == 0
-        if(i == n){
+    void helper(List<List<Integer>> result, List<Integer> list, int[] nums, int cur, boolean taken){
+        if(cur == nums.length){
             result.add(new ArrayList<>(list));
             return;
         }
-        if(i == 0 || nums[i] != nums[i - 1] || c == true){
-            solve(result, list, nums, n, i + 1, false);
-            list.add(nums[i]);
-            solve(result, list, nums, n, i + 1, true);
-            list.remove(list.size() - 1);    
-        } else{ // this element is same as previous element, if we chose that, we'll choose this, and vv
-            solve(result, list, nums, n, i + 1, false);
+        if(cur > 0 && nums[cur] == nums[cur - 1]){
+            if(taken){
+                list.add(nums[cur]);
+                helper(result, list, nums, cur + 1, true);
+                list.remove(list.size() - 1);
+            }
+        } else{
+            list.add(nums[cur]);
+            helper(result, list, nums, cur + 1, true);
+            list.remove(list.size() - 1);
         }
+        helper(result, list, nums, cur + 1, false);
+        
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
-        solve(result, new ArrayList<>(), nums, nums.length, 0, false);
+        helper(result, new ArrayList<>(), nums, 0, false);
         return result;
     }
 }
