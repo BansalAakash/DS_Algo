fad1d7d766cfcd02c39a4fbf7491d27edc4a2c5a
Aakash Bansal <aakash6025@gmail.com>
2022-09-20 16:46:51 +0530

    Time: 2 ms (92.40%), Space: 41.1 MB (82.98%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    private String getPermutation(int n, int k, HashSet<Integer> set, StringBuilder string, int[] fact){
+        int firstI = 1;
+        for(firstI = 1;firstI <= 9;firstI++)
+            if(!set.contains(firstI)) break;
+        if(n == 1){
+            string.append(firstI);
+            return string.toString();
+        }
+        int i = firstI, temp = 0, count = -1;
+        while(temp < k){
+            if(!set.contains(i)){
+                temp += fact[n - 1];
+                count++;
+            }
+            i++;
+        }
+        set.add(i - 1);
+        string.append(i - 1);
+        return getPermutation(n - 1, k - count * fact[n - 1], set, string, fact);
+    }
+    public String getPermutation(int n, int k) {
+        int[] fact = new int[n + 1];
+        fact[0] = 1;
+        for(int i = 1;i <= n;i++) fact[i] = i * fact[i - 1];
+        return getPermutation(n, k, new HashSet<>(), new StringBuilder(), fact);
+    }
+}
