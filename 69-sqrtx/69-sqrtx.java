6e81690b367d941c597a0d5fbee400f54fa190f2
Aakash Bansal <aakash6025@gmail.com>
2022-09-23 18:26:53 +0530

    Time: 3 ms (42.22%), Space: 41.5 MB (34.75%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -9,7 +9,7 @@ class Solution {
             if(mid > x / mid)
                 high = mid - 1;
             else {
-                if(mid == x / mid || (mid + 1) > x / (mid + 1)) return mid;
+                if((mid + 1) > x / (mid + 1)) return mid;
                 else low = mid + 1;
             }
         }
