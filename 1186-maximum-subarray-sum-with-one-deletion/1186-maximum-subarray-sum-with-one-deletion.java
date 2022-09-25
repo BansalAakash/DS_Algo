e030477dd4b9040b4db6148d715f4c8e00d91d67
Aakash Bansal <aakash6025@gmail.com>
2022-09-25 15:06:21 +0530

    Time: 8 ms (46.09%), Space: 58.5 MB (71.09%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
@@ -1,21 +1,16 @@
 class Solution {
     public int maximumSum(int[] arr) {
-        int n = arr.length;
-        if(n == 1)
-            return arr[0];
+        int n = arr.length, max = arr[0];
         int left[] = new int[n], right[] = new int[n];
-        
+        left[0] = arr[0];
+        right[n - 1] = arr[n - 1];
         for(int i = 1;i < n;i++){
-            left[i] = arr[i - 1] + Math.max(0, left[i - 1]);                //left subarray ending at i - 1
-            right[n - i - 1] = arr[n - i] + Math.max(0, right[n - i]);  //right subarray ending at i + 1
+            left[i] = arr[i] + Math.max(0, left[i - 1]);
+            right[n - i - 1] = arr[n - i - 1] + Math.max(0, right[n - i]);
+            max = Math.max(max, left[i]);                                   // No deletions
         }
-        int max = right[0] + Math.max(0, arr[0]);           //for first elem only consider right subarray
-        for(int i = 1;i < n - 1;i++){                        //go from second to last second elem
-            int temp = Math.max(0, arr[i]);
-            max = Math.max(max, Math.max(left[i] + right[i] + temp, Math.max(left[i] + temp, right[i] + temp)));
-            
-        }
-        max = Math.max(max, left[n - 1] + Math.max(0, arr[n - 1]));//for last elem only consider left subarray
+        for(int i = 1;i < n - 1;i++)
+            max = Math.max(max, left[i - 1] + right[i + 1]);
         return max;
     }
 }
