e2987c502e89921bf091800497e698e82b95d537
Aakash Bansal <aakash6025@gmail.com>
2022-09-26 11:43:35 +0530

    Time: 2 ms (14.99%), Space: 43.4 MB (73.64%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,38 +1,22 @@
 class Solution {
-    boolean binarySearch(int[] nums, int low, int high, int target){
-        while(low <= high){
-            int mid = low + (high - low) / 2;
+    public boolean search(int[] nums, int target) {
+        int low = 0, n = nums.length, high = n - 1, mid = -1;
+        while(low < high){
+            mid = low + (high - low) / 2;
             if(nums[mid] == target)
                 return true;
-            if(nums[mid] > target) high = mid - 1;
-            else low = mid + 1;
-        }
-        return false;
-    }
-    public boolean search(int[] nums, int target) {
-        int n = nums.length;
-        if(n == 0) return nums[0] == target;
-        if(nums[0] >= nums[n - 1]){
-            int pivot = -1, low = 0, high = n - 1;
-            while(low < high){
-                int mid = low + (high - low) / 2;
-                if(nums[mid] < nums[high])
-                    high = mid;
-                else if(nums[mid] > nums[high])
+            if(nums[mid] < nums[high]){         //right is sorted
+                if(target > nums[mid] && target <= nums[high])
                     low = mid + 1;
-                else{
-                    if(nums[high] < nums[high - 1]){
-                        pivot = high;
-                        break;
-                    } else 
-                        high--;
-                }
+                else high = mid - 1;
             }
-            pivot = pivot == -1 ? low : pivot;
-            if(target >= nums[0])
-                return binarySearch(nums, 0, pivot, target);
-            else return binarySearch(nums, pivot, n - 1, target);
-        } else return binarySearch(nums, 0, n - 1, target);
-        
+            else if(nums[mid] > nums[high]){         //left is sorted
+                if(target >= nums[low] && target < nums[mid])
+                    high = mid - 1;
+                else low = mid + 1;
+            }
+            else high--;
+        }
+        return nums[low] == target;
     }
 }
