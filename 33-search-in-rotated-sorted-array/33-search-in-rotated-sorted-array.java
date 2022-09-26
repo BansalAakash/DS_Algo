3d9ba3e198eff2a4fb061bf41c1e240310e6a93b
Aakash Bansal <aakash6025@gmail.com>
2022-09-26 10:43:52 +0530

    Time: 1 ms (78.35%), Space: 42.6 MB (54.41%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,29 +1,21 @@
 class Solution {
-    int findPivot(int[] nums){
+    public int search(int[] nums, int target) {
         int n = nums.length, low = 0, high = n - 1, mid = -1;
-        if(n == 1 || nums[0] < nums[n - 1])
-            return 0;
-        while(low < high){
-            mid = low + (high - low) / 2;
-            if(nums[mid] > nums[n - 1]) low = mid + 1;
-            else high = mid;
-        }
-        return low;
-    }
-    int binarySearch(int[] nums, int target, int low, int high){
-        int mid = -1;
         while(low <= high){
             mid = low + (high - low) / 2;
-            if(nums[mid] == target) return mid;
-            if(nums[mid] > target) high = mid - 1;
-            else low = mid + 1;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[low] <= nums[mid]){          //left is sorted
+                if(target >= nums[low] && target < nums[mid])
+                    high = mid - 1;
+                else low = mid + 1;
+            }
+            else {          //right is sorted
+                if(target <= nums[high] && target > nums[mid])
+                    low = mid + 1;
+                else high = mid - 1;
+            }
         }
         return -1;
-    }
-    public int search(int[] nums, int target) {
-        int pivot = findPivot(nums), n = nums.length;
-        if(target >= nums[pivot] && target <= nums[n - 1])
-            return binarySearch(nums, target, pivot, n - 1);
-        return binarySearch(nums, target, 0, pivot - 1);
     }
 }
