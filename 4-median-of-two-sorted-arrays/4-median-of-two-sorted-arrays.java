5ae65f0551d95b03b3e3dc30af0c0df4d7de3952
Aakash Bansal <aakash6025@gmail.com>
2022-09-27 09:58:48 +0530

    Time: 3 ms (89.67%), Space: 49.7 MB (64.00%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,26 +1,26 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i = 0, j = 0, n1 = nums1.length, n2 = nums2.length, n = n1 + n2, count = 0;
-        int elem1 = 0, elem2 = 0;
-        while(count++ <= n / 2){
-            elem1 = elem2;
-            if(i == n1){
-                elem2 = nums2[j];
-                j++;
-            }
-            else if(j == n2){
-                elem2 = nums1[i];
-                i++;
-            }
-            else if(nums1[i] < nums2[j]){
-                elem2 = nums1[i];
-                i++;
-            }
-            else{
-                elem2 = nums2[j];
-                j++;
-            }
+        int n1 = nums1.length, n2 = nums2.length, n = n1 + n2;
+        if(n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        int cut1 = 0, cut2 = 0, l1 = 0, l2 = 0, r1 = 0, r2 = 0;
+        int x = n % 2 == 0 ? n / 2 : n / 2 + 1;
+        int low = 0, high = Math.min(n1, x);
+        while(low <= high){
+            cut1 = low + (high - low) / 2;
+            l1 = cut1 > 0 ? nums1[cut1 - 1] : Integer.MIN_VALUE;
+            r1 = cut1 < n1 ? nums1[cut1] : Integer.MAX_VALUE;
+            
+            cut2 = x - cut1;
+            l2 = cut2 > 0 ? nums2[cut2 - 1] : Integer.MIN_VALUE;
+            r2 = cut2 < n2 ? nums2[cut2] : Integer.MAX_VALUE;
+            
+            if(l1 <= r2 && l2 <= r1)
+                break;
+            if(l1 > r2)
+                high = cut1 - 1;
+            else low = cut1 + 1;
         }
-        return n % 2 == 1 ? elem2 : (elem1 + elem2) / 2.0;
+        return n % 2 == 0 ? (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0 : Math.max(l1, l2);
     }
 }
