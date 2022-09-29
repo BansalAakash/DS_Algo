eb0152c86a7d993859e7589178436c66f45a6f9d
Aakash Bansal <aakash6025@gmail.com>
2022-09-29 11:01:39 +0530

    Time: 29 ms (73.93%), Space: 76.5 MB (19.72%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -15,17 +15,16 @@ class Solution {
         swap(nums, low, j);                                     //j is the position of the pivot
         return j;
     }
-    int quickSelect(int[] nums, int low, int high, int k){
-        int pivot = partition(nums, low, high);
-        if(pivot == k)
-            return nums[pivot];
-        if(pivot < k)
-            return quickSelect(nums, pivot + 1, high, k);
-        return quickSelect(nums, low, pivot - 1, k);
-    }
     public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
+        int n = nums.length, low = 0, high = n - 1, pivot = -1;
         k = n - k;             //kth largest means we need to return (n - k)th element
-        return quickSelect(nums, 0, n - 1, k);
+        
+        while(low <= high){
+            pivot = partition(nums, low, high);
+            if(pivot == k) break;
+            if(pivot < k) low = pivot + 1;
+            else high = pivot - 1;
+        }
+        return nums[k];
     }
 }
