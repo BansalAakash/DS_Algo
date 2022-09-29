10270ffd55071588928aee8697dba4ef41f46fa4
Aakash Bansal <aakash6025@gmail.com>
2022-09-29 10:56:30 +0530

    Time: 31 ms (71.42%), Space: 75.8 MB (27.28%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,11 +1,31 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        for(int i : nums){
-            q.add(i);
-            if(q.size() > k)
-                q.poll();
+    void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    int partition(int[] nums, int low, int high){               //we'll take low as pivot
+        int i = low, j = high;
+        while(i < j){
+            while(i <= high && nums[i] <= nums[low]) i++;       //i goes from pivot + 1 to high
+            while(j >= low && nums[j] > nums[low]) j--;          //j goes from high to pivot
+            if(i < j)
+                swap(nums, i, j);
         }
-        return q.poll();
+        swap(nums, low, j);                                     //j is the position of the pivot
+        return j;
+    }
+    int quickSelect(int[] nums, int low, int high, int k){
+        int pivot = partition(nums, low, high);
+        if(pivot == k)
+            return nums[pivot];
+        if(pivot < k)
+            return quickSelect(nums, pivot + 1, high, k);
+        return quickSelect(nums, low, pivot - 1, k);
+    }
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        k = n - k;             //kth largest means we need to return (n - k)th element
+        return quickSelect(nums, 0, n - 1, k);
     }
 }
