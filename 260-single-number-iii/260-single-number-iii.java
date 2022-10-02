fa1f4b287761f0158b1e4f57679b3f7995f501a5
Aakash Bansal <aakash6025@gmail.com>
2022-10-02 21:38:42 +0530

    Time: 1 ms (100.00%), Space: 45.5 MB (32.39%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        if(nums.length == 2)
+            return nums;
+        int result = 0;
+        for(int i : nums)
+            result ^= i;
+        int i = 0;
+        while((1 & (result >> i)) == 0)
+            i++;
+        int part1 = 0, part2 = 0;
+        for(int j : nums){
+            if((1 & (j >> i)) == 1)
+                part1 ^= j;
+            else part2 ^= j;
+        }
+        return new int[] {part1 ^ result, part2 ^ result};
+    }
+}
