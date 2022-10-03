e78b18c70f2e4be87008402aca2b67ff93848335
Aakash Bansal <aakash6025@gmail.com>
2022-10-03 10:01:57 +0530

    Time: 38 ms (65.71%), Space: 55.3 MB (24.04%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -1,20 +1,19 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int max = 0, n = nums.length;
-        for(int i = 1; i < n;i++)
-            if(nums[i] > nums[max])
-                max = i;
+        int n = nums.length;
         Stack<Integer> stack = new Stack<>();
-        int[] result = new int[n];
-        int i = max, count = 0;
-        while(count <= n){
-            while(!stack.isEmpty() && nums[stack.peek()] <= nums[i]) stack.pop();
-            if(stack.isEmpty()) result[i] = -1; 
-            else result[i] = nums[stack.peek()];
-            // System.out.println("result[" + i + "] is " + result[i]);
-            stack.push(i);
-            i = (i > 0) ? i - 1 : n - 1;
-            count++;
+        int[] result = new int[nums.length];
+        
+        for(int i = n - 1;i >= 0;i--)
+            stack.push(nums[i]);
+        
+        for(int i = n - 1;i >= 0;i--){
+            while(!stack.isEmpty() && stack.peek() <= nums[i])
+                stack.pop();
+            if(stack.isEmpty())
+                result[i] = -1;
+            else result[i] = stack.peek();
+            stack.push(nums[i]);
         }
         return result;
     }
