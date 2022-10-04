d2c85a0c09076f5d4df9210d94f8cf03dc7a0089
Aakash Bansal <aakash6025@gmail.com>
2022-10-04 09:15:35 +0530

    Time: 311 ms (27.68%), Space: 95.4 MB (32.73%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,32 +1,32 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        if(heights.length == 0) return 0;
-        int n = heights.length;
-        int[] leftSmaller = new int[n];
-        int[] rightSmaller = new int[n];
-        leftSmaller[0] = -1;
-        rightSmaller[n - 1] = n;
-
-        //Calculating leftSmaller and rightSmaller
-        Stack<Integer> leftStack = new Stack<>();
-        leftStack.push(0);
-        Stack<Integer> rightStack = new Stack<>();
-        rightStack.push(n - 1);
-        for(int i = 1;i < n;i++){
-            while(!rightStack.isEmpty() && heights[rightStack.peek()] >= heights[n - i - 1]) rightStack.pop();
-            if(rightStack.isEmpty()) rightSmaller[n - i - 1] = n;
-            else rightSmaller[n - i - 1] = rightStack.peek();
-            rightStack.push(n - i - 1);
-
-            while(!leftStack.isEmpty() && heights[leftStack.peek()] >= heights[i]) leftStack.pop();
-            if(leftStack.isEmpty()) leftSmaller[i] = -1;
-            else leftSmaller[i] = leftStack.peek();
-            leftStack.push(i);
+    public int largestRectangleArea(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Stack<Integer> lstack = new Stack<>();
+        for(int i = 0;i < n;i++){
+            while(!lstack.isEmpty() && nums[lstack.peek()] >= nums[i])
+                lstack.pop();
+            if(lstack.isEmpty())
+                left[i] = -1;
+            else left[i] = lstack.peek();
+            lstack.push(i);
         }
-        int maxArea = 0;
+        
+        Stack<Integer> rstack = new Stack<>();
+        for(int i = n - 1;i >= 0;i--){
+            while(!rstack.isEmpty() && nums[rstack.peek()] >= nums[i])
+                rstack.pop();
+            if(rstack.isEmpty())
+                right[i] = n;
+            else right[i] = rstack.peek();
+            rstack.push(i);
+        }
+        // System.out.println(Arrays.toString(left));
+        // System.out.println(Arrays.toString(right));
+        int max = 0;
         for(int i = 0;i < n;i++)
-            maxArea = Math.max(maxArea, heights[i] * (rightSmaller[i] - leftSmaller[i] - 1));
-        return maxArea;
-
+            max = Math.max((right[i] - left[i] - 1) * nums[i], max);
+        return max;
     }
 }
