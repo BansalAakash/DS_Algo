ae73dcac964c3f3a98788d18ddd996fb09ce1411
Aakash Bansal <aakash6025@gmail.com>
2022-10-04 11:42:36 +0530

    Time: 93 ms (23.53%), Space: 154.5 MB (28.91%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -4,7 +4,7 @@ class Solution {
         ArrayDeque<Integer> dq = new ArrayDeque<>();
         int[] result = new int[n - k + 1];
         for(int i = 0;i < n;i++){
-            while(!dq.isEmpty() && dq.getFirst() < i - k + 1)               //remove expired elems from front
+            if(!dq.isEmpty() && dq.getFirst() < i - k + 1)               //remove expired elems from front
                 dq.removeFirst();
             while(!dq.isEmpty() && nums[dq.getLast()] < nums[i])                  //remove small elems from rear
                 dq.removeLast();
