1065122d83c02e01adca4951aaa70fe5624e8daa
Aakash Bansal <aakash6025@gmail.com>
2022-10-04 17:57:01 +0530

    Time: 5 ms (27.27%), Space: 43.1 MB (36.02%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,34 +1,51 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        Queue<int[]> q = new LinkedList<>();
-        int m = grid.length, n = grid[0].length, countFresh = 0;
-        for(int i = 0;i < m;i++){
-            for(int j = 0;j < n;j++){
-                if(grid[i][j] == 2) 
-                    q.add(new int[] {i, j});
-                else if(grid[i][j] == 1) countFresh++;
-            }
+    class Node{
+        int i, j;
+        public Node(int i, int j){
+            this.i = i;
+            this.j = j;
         }
-        if(countFresh == 0) return 0;
-        int mins = 0;
-        int[][] dir = {{-1,0},{1,0},{0,-1},{0,1}};
-        while(!q.isEmpty()){
-            int size = q.size(), count = 0, foundSomething = 0;
-            // display(q);
-            while(count++ < size){
-                int row = q.peek()[0], col = q.poll()[1];
-                for(int i = 0;i < 4;i++){
-                    int newRow = row + dir[i][0], newCol = col + dir[i][1];
-                    if(newRow >= 0 && newCol >= 0 && newRow < m && newCol < n && grid[newRow][newCol] == 1){
-                        grid[newRow][newCol] = 2;
-                        q.add(new int[]{newRow, newCol});
-                        foundSomething = 1;
-                        countFresh--;
-                    }
+    }
+    int mark(int[][] grid, boolean[][] visited, Queue<Node> rotten, int countFresh){
+        int mins = 0, m = grid.length, n = grid[0].length;
+        while(!rotten.isEmpty()){
+            int size = rotten.size();
+            boolean flag = false;
+            while(size-- > 0){
+                Node temp = rotten.poll();
+                int i = temp.i, j = temp.j;
+                visited[i][j] = true;
+                int[] dirs = {0, -1, 0, 1, 0};
+                for(int x = 0;x < 4;x++){
+                    int x_ = i + dirs[x], y_ = j + dirs[x + 1];
+                    if(x_ < 0 || y_ < 0 || x_ >= m || y_ >= n || grid[x_][y_] != 1 || visited[x_][y_])
+                        continue;
+                    flag = true;
+                    visited[x_][y_] = true;
+                    countFresh--;
+                    grid[x_][y_] = 2;
+                    rotten.add(new Node(x_, y_));
                 }
             }
-            if(foundSomething == 1) mins++;
+            if(flag)
+                mins++;
+            // System.out.println("Minutes : " + mins);
+            // display(grid);
+            // displayQueue(rotten);
         }
-        return countFresh == 0 ? mins : -1;
+        return countFresh > 0 ? -1 : mins;
+    }
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        Queue<Node> q = new LinkedList<>();
+        int countFresh = 0;
+        for(int i = 0;i < m;i++)
+            for(int j = 0;j < n;j++)
+                if(grid[i][j] == 2)
+                    q.add(new Node(i, j));
+                else if(grid[i][j] == 1)
+                    countFresh++;
+        return mark(grid, visited, q, countFresh);
     }
 }
