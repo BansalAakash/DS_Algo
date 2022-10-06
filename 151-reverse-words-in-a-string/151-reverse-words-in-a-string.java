415ec3d0117d581a0d58fd82d0ff8b2baef84b4e
Aakash Bansal <aakash6025@gmail.com>
2022-10-06 10:16:33 +0530

    Time: 11 ms (65.43%), Space: 42.4 MB (90.35%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,16 +1,41 @@
 class Solution {
-    public String reverseWords(String s) {
-        StringBuilder str = new StringBuilder();
-        int n = s.length(), i = n - 1, j = 0;
-        while(i >= 0){
-            while(i >= 0 && s.charAt(i) == ' ') i--;
-            while(i >= 0 && s.charAt(i) != ' ')
-                str.insert(j, s.charAt(i--));
-            str.append(' ');
-            j = str.length();
-            while(i >= 0 && s.charAt(i) == ' ') i--;
+    void reverseString(int i, int j, char[] ch){
+        while(i < j){
+            char temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+            i++;
+            j--;
         }
-        str.delete(str.length() - 1, str.length());
-        return str.toString();
+    }
+    public String reverseWords(String s) {
+        char[] ch = s.toCharArray();
+        int i = 0, j = 0, n = s.length();
+        //clean spaces - j remains ahead
+        while(j < n){
+            while(j < n && ch[j] == ' ') j++;
+            while(j < n && ch[j] != ' ') ch[i++] = ch[j++];
+            while(j < n && ch[j] == ' ') j++;
+            if(j < n) ch[i++] = ' ';
+        }
+        //i is at 1 + last index of the string without trailing spaces
+        n = i - 1;
+        // System.out.println("After cleaning spaces ; " + Arrays.toString(ch));
+        
+        //reverse whole string
+        reverseString(0, n, ch);
+        // System.out.println("After reversing whole ; " + Arrays.toString(ch));
+        
+        
+        //reverse individual words within string
+        i = 0;
+        j = 0;
+        while(j <= n){
+            while(j <= n && ch[j] != ' ')j++;
+            reverseString(i, j - 1, ch);
+            j++;
+            i = j;
+        }
+        return new String(ch, 0, n + 1);
     }
 }
