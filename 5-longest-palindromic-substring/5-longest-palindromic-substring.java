4d834e48aee30dc3747ba5fdc82736dbbb1254ca
Aakash Bansal <aakash6025@gmail.com>
2022-10-06 11:38:00 +0530

    Time: 31 ms (73.29%), Space: 42.2 MB (90.87%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,27 +1,25 @@
 class Solution {
-    int len(String s, int i, int j){
-        while(i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)){
-            i--;
-            j++;
-        }
-        return j - i - 1;
+    int checkPalindrome(String s, int x, int y, int n){
+        if(y >= n) return 0;
+        while(x >= 0 && y < n && s.charAt(x) == s.charAt(y)){x--;y++;}
+        return y - x - 1;
     }
     public String longestPalindrome(String s) {
-        int left = 0, right = 0, max = 0;
-        for(int i = 0;i < s.length();i++){
-            int tempOdd = len(s, i, i);
-            if(tempOdd > max){
-                max = tempOdd;
-                left = i - max / 2;
-                right = i + max / 2;
+        int n = s.length(), max = 0, start = 0, end = 0;
+        for(int i = 0;i < n;i++){
+            int x = checkPalindrome(s, i, i, n), y = checkPalindrome(s, i, i + 1, n);
+            // System.out.println("i : " + i + ", x : " + x + ", y : " + y);
+            if(x > max){
+                max = x;
+                start = i - x / 2;
+                end = i + x / 2;
             }
-            int tempEven = len(s, i, i + 1);
-            if(tempEven > max){
-                max = tempEven;
-                left = i - max / 2 + 1;
-                right = i + max / 2;
+            if(y > max){
+                max = y;
+                start = i - (y / 2 - 1);
+                end = i + 1 + (y / 2 - 1);
             }
         }
-        return s.substring(left, right + 1);
+        return s.substring(start, end + 1);
     }
 }
