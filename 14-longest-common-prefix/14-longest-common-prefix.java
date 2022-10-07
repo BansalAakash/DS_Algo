5443535d94d22f75ce3bb06a22c0789fa8b669cc
Aakash Bansal <aakash6025@gmail.com>
2022-10-07 12:28:41 +0530

    Time: 2 ms (53.45%), Space: 42.3 MB (43.65%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int in = 0, n = strs.length;
+        char ch = ' ';
+        while(in < strs[0].length()){
+            ch = strs[0].charAt(in);
+            for(int i = 1;i < n;i++)
+                if(in == strs[i].length() || strs[i].charAt(in) != ch)
+                    return strs[0].substring(0, in);
+            in++;
+        }
+        return strs[0];
+    }
+}
