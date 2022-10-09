e134d42367056ef0b68dcb009d472d2c610183e2
Aakash Bansal <aakash6025@gmail.com>
2022-10-09 16:43:05 +0530

    Time: 1 ms (67.62%), Space: 41.8 MB (70.32%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,38 +1,40 @@
+import java.util.*;
 class Solution{
+	int[] lps(String pat){
+		int n = pat.length(), i = 1, j = 0;
+		int[] lps = new int[n];
+		while(i < n){
+			if(pat.charAt(i) == pat.charAt(j)){
+				while(i < n && pat.charAt(i) == pat.charAt(j)){
+					lps[i] = j + 1;
+					i++;
+					j++;
+				}
+				j = lps[Math.max(0, j - 1)];
+			}
+			else
+				i++;
+		}
+		// System.out.println(Arrays.toString(lps));
+		return lps;
+	}
 	public int strStr(String str, String pat){
 		if(pat.length() > str.length())
 			return -1;
-		String s = pat + "$" + str;
-		int left = 0, right = 0, n = s.length();
-		// int comp = 0;						//To track number of comparisons
-		int[] z = new int[s.length()];
-		for(int i = 1;i < n;i++){				//we start with first index cuz 0th index's z is always 0
-			if(right < i){						//we are not inside z-box
-				left = i;
-				right = i;
-				while(right < n && s.charAt(right) == s.charAt(right - left)){
-					// comp++;
-					right++;
+		int[] lps = lps(pat);
+		int i = 0, j = 0, m = str.length(), n = pat.length();
+		while(i < m){
+			if(str.charAt(i) == pat.charAt(j))
+				while(i < m && j < n && str.charAt(i) == pat.charAt(j)){
+					i++;
+					j++;
 				}
-				z[i] = right - left;
-				right--;
-			} else{								//we are inside z-box
-				z[i] = Math.min(z[i - left], right - i + 1);
-				if(z[i - left] >= right - i + 1){							//The end of current character's coinciding with the right of current z box
-					left = i;right = left + z[i];
-					while(right < n && s.charAt(right) == s.charAt(right - left)){
-						// comp++;
-						right++;
-					}
-					z[i] = right - left;
-					right--;
-				}
-			}
+			else if(j == 0)
+				i++;
+			if(j == n)
+				return i - j;
+			j = lps[Math.max(0, j - 1)];
 		}
-		// System.out.println("Did " + comp + " comparisons for length " + n);
-		for(int i = 0;i < n;i++)
-			if(z[i] == pat.length())
-				return i - pat.length() - 1;
 		return -1;
 	}
 }
