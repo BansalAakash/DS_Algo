13ac15ebebfb7f74f1a05bb652354c718ce6cbbe
Aakash Bansal <aakash6025@gmail.com>
2022-10-13 16:25:57 +0530

    Time: 1 ms (96.92%), Space: 43 MB (33.57%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -14,28 +14,22 @@
  * }
  */
 class Solution {
-    void helper(TreeNode root, List<LinkedList<Integer>> list, int level, boolean flag){ //true means left to right, false means vv
+    void helper(TreeNode root, List<List<Integer>> list, int level, boolean flag){ //true means left to right, false means vv
         if(root == null) return;
         
         if(list.size() <= level)
             list.add(new LinkedList<>());
         
         if(flag)                           //l2r
-            list.get(level).addFirst(root.val);
+            ((LinkedList<Integer>)list.get(level)).addFirst(root.val);
         else                                //r2l
             list.get(level).add(root.val);
         helper(root.right, list, level + 1, !flag);
         helper(root.left, list, level + 1, !flag);
     }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<LinkedList<Integer>> list = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
         helper(root, list, 0, true);
-        List<List<Integer>> result = new ArrayList<>();
-        for(LinkedList<Integer> cur : list){
-            List<Integer> temp = new ArrayList<>();
-            for(int i : cur) temp.add(i);
-            result.add(temp);
-        }
-        return result;
+        return list;
     }
 }
