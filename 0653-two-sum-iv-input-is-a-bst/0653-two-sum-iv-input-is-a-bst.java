be70948142dd7ba7f22427bec12ef566a1fee18b
Aakash Bansal <aakash6025@gmail.com>
2022-10-20 09:41:58 +0530

    Time: 6 ms (69.46%), Space: 50 MB (79.41%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,16 +14,29 @@
  * }
  */
 class Solution {
-    private boolean helper(TreeNode root, int k, HashSet<Integer> set){
-        if(root == null)
-            return false;
-        if(set.contains(k - root.val))
-            return true;
-        set.add(root.val);
-        return helper(root.left, k, set) | helper(root.right, k, set);
+    TreeNode find(TreeNode root, int val) {
+        if(root == null || val == root.val)
+            return root;
+        if(val < root.val)
+            return find(root.left, val);
+        return find(root.right, val);
     }
     public boolean findTarget(TreeNode root, int k) {
-        HashSet<Integer> set = new HashSet<>();
-        return helper(root, k, set);
+        if(root == null)
+            return false;
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            TreeNode find_ = find(root, k - cur.val);
+            if(find_ != null && find_ != cur)
+                return true;
+            cur = cur.right;
+        }
+        return false;
     }
 }
