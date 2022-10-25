25ac92bfe4b0fd5545d5bdb97ed063189cb92465
Aakash Bansal <aakash6025@gmail.com>
2022-10-25 12:44:20 +0530

    Time: 1 ms (90.33%), Space: 47.6 MB (77.80%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,19 +1,17 @@
 class Solution {
-    private void fill(int[][] image, int m, int n, int orig, int i, int j, int c, int[][] visited){
-        if(i >= m || j >= n || i < 0 || j < 0 || visited[i][j] == 1 || image[i][j] != orig)
+    private void fill(int[][] image, int m, int n, int orig, int i, int j, int c){
+        if(i >= m || j >= n || i < 0 || j < 0 || image[i][j] == c || image[i][j] != orig)
             return;
-        visited[i][j] = 1;
         image[i][j] = c;
         int[] dirs = {-1, 0, 1, 0, -1};
         for(int i_ = 0;i_ < 4;i_++)
-            fill(image, m, n, orig, i + dirs[i_], j + dirs[i_ + 1], c, visited);
+            fill(image, m, n, orig, i + dirs[i_], j + dirs[i_ + 1], c);
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         if(image[sr][sc] == color)
             return image;
         int m = image.length, n = image[0].length;
-        int[][] visited = new int[m][n];
-        fill(image, m, n, image[sr][sc], sr, sc, color, visited);
+        fill(image, m, n, image[sr][sc], sr, sc, color);
         return image;
     }
 }
