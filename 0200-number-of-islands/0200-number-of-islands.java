cf847c070c794bae89904fcb99c684873bda6515
Aakash Bansal <aakash6025@gmail.com>
2022-10-25 14:56:33 +0530

    Time: 10 ms (31.41%), Space: 57.2 MB (62.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,19 +1,18 @@
 class Solution {
     private static final int[] dirs = {-1, 0, 1, 0, -1};
-    private boolean helper(char[][] grid, int[][] visited, int i, int j, int m, int n){
-        if(i < 0 || j < 0 || i == m || j == n || visited[i][j] == 1 || grid[i][j] == '0')
+    private boolean helper(char[][] grid, int i, int j, int m, int n){
+        if(i < 0 || j < 0 || i == m || j == n || grid[i][j] == '0')
             return false;
-        visited[i][j] = 1;
+        grid[i][j] = '0';
         for(int i_ = 0; i_ < 4; i_++)
-            helper(grid, visited, i + dirs[i_], j + dirs[i_ + 1], m, n);
+            helper(grid, i + dirs[i_], j + dirs[i_ + 1], m, n);
         return true;
     }
     public int numIslands(char[][] grid) {
         int m = grid.length, n = grid[0].length, result = 0;
-        int[][] visited = new int[m][n];
         for(int i = 0;i < m;i++)
             for(int j = 0;j < n;j++)
-                if(helper(grid, visited, i, j, m, n))
+                if(helper(grid, i, j, m, n))
                     result++;
         return result;
     }
