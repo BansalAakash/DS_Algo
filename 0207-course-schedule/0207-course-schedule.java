4ac18fa19bcf9f4055b5670dc13bf24bfbdf882b
Aakash Bansal <aakash6025@gmail.com>
2022-10-26 17:39:41 +0530

    Time: 12 ms (54.89%), Space: 48.7 MB (35.92%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,30 +1,31 @@
 import java.util.*;
 class Solution {
-    boolean dfs(int src, HashMap<Integer, ArrayList<Integer>> graph, int[] visited){
-        if(visited[src] == 2)
-            return true;
-        if(visited[src] == 1)
-            return false;
-        visited[src] = 1;
-        for(int i : graph.get(src)){
-            if(!dfs(i, graph, visited))
-                return false;
+    boolean bfs(HashMap<Integer, ArrayList<Integer>> graph, int[] indegrees, int numCourses){
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0;i < numCourses;i++)
+            if(indegrees[i] == 0)
+                q.add(i);
+        ArrayList<Integer> result = new ArrayList<>();
+        while(!q.isEmpty()){
+            int node = q.poll();
+            result.add(node);
+            for(int j : graph.get(node)){
+                indegrees[j]--;
+                if(indegrees[j] == 0)
+                    q.add(j);
+            }
         }
-        visited[src] = 2;
-        return true;
+        return result.size() == numCourses;
     }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
         for(int i = 0;i < numCourses;i++)
             graph.put(i, new ArrayList<>());
-        for(int[] row : prerequisites)
+        int[] indegrees = new int[numCourses];
+        for(int[] row : prerequisites){
             graph.get(row[1]).add(row[0]);
-        int[] visited = new int[numCourses];                // 0 means not visited, 1 means being visited, 2 means completed.
-        for(int i = 0;i < numCourses;i++){
-            if(!dfs(i, graph, visited))
-                return false;
+            indegrees[row[0]]++;
         }
-
-        return true;
+        return bfs(graph, indegrees, numCourses);
     }
 }
