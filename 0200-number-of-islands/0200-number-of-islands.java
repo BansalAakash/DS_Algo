4774d9b6da369d04f62aa72e68260c2b7e1d5c31
Aakash Bansal <aakash6025@gmail.com>
2022-10-28 11:49:20 +0530

    Time: 10 ms (32.10%), Space: 56.2 MB (77.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,19 +1,31 @@
 class Solution {
     private static final int[] dirs = {-1, 0, 1, 0, -1};
-    private boolean helper(char[][] grid, int i, int j, int m, int n){
-        if(i < 0 || j < 0 || i == m || j == n || grid[i][j] == '0')
-            return false;
-        grid[i][j] = '0';
-        for(int i_ = 0; i_ < 4; i_++)
-            helper(grid, i + dirs[i_], j + dirs[i_ + 1], m, n);
-        return true;
+    void bfs(int row, int col, char[][] grid, int m, int n){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{row, col});
+        grid[row][col] = '0';
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int i = cur[0], j = cur[1];
+            for(int k = 0;k < 4;k++){
+                int r = i + dirs[k], c = j + dirs[k + 1];
+                if(r >= 0 && c >= 0 && r < m && c < n && grid[r][c] == '1'){
+                    q.add(new int[] {r, c});
+                    grid[r][c] = '0';
+                }
+            }
+        }
     }
     public int numIslands(char[][] grid) {
         int m = grid.length, n = grid[0].length, result = 0;
-        for(int i = 0;i < m;i++)
-            for(int j = 0;j < n;j++)
-                if(helper(grid, i, j, m, n))
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(grid[i][j] == '1'){
+                    bfs(i, j, grid, m, n);
                     result++;
+                }
+            }
+        }
         return result;
     }
 }
