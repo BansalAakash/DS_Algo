5b830b10b2adccb7d57bc4d03ac8dd78d5c53e7d
Aakash Bansal <aakash6025@gmail.com>
2022-11-10 20:11:25 +0530

    Time: 14 ms (48.03%), Space: 88 MB (35.56%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,21 +1,14 @@
 class Solution {
-    int f(int i, int[] prices, int n, int cap, int canBuy, int[][][] dp){
-        if(i == n || cap == 2)
-            return 0;
-        if(dp[canBuy][cap][i] != -1)
-            return dp[canBuy][cap][i];
-        int profit = 0;
-        if(canBuy == 1)
-            profit = Math.max(f(i + 1, prices, n, cap, 0, dp) - prices[i], f(i + 1, prices, n, cap, 1, dp));
-        else profit = Math.max(f(i + 1, prices, n, cap + 1, 1, dp) + prices[i], f(i + 1, prices, n, cap, 0, dp));
-        return dp[canBuy][cap][i] = profit;
-    }
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][][] dp = new int[2][3][n + 1];
-        for(int[][] i : dp)
-            for(int j[] : i)
-                Arrays.fill(j, -1);
-        return f(0, prices, n, 0, 1, dp);
+        
+        for(int cap = 1;cap >= 0;cap--){
+            for(int i = n - 1;i >= 0;i--){
+                dp[1][cap][i] = Math.max(dp[0][cap][i + 1] - prices[i], dp[1][cap][i + 1]);
+                dp[0][cap][i] = Math.max(dp[1][cap + 1][i + 1] + prices[i], dp[0][cap][i + 1]);
+            }
+        }
+        return dp[1][0][0];
     }
 }
