a60212b211a62d5ce8b97dbd976f0f5db2ead40f
Aakash Bansal <aakash6025@gmail.com>
2022-11-10 07:27:58 +0530

    Time: 59 ms (26.88%), Space: 48.8 MB (39.29%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,29 +1,18 @@
 class Solution {
-    boolean f(int i, int j, String s, String p, int[][] dp){
-        if(i == -1){
-            if(j == -1)
-                return true;
-            if(p.charAt(j) == '*')
-                return f(i, j - 1, s, p, dp);
-            return false;
-        }
-        if(j == -1)
-            return false;
-        if(dp[i][j] != -1)
-            return dp[i][j] == 0 ? false : true;
-        boolean result = false;
-        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')
-            result = f(i - 1, j - 1, s, p, dp);
-        if(p.charAt(j) == '*')
-            result = f(i - 1, j, s, p, dp) | f(i, j - 1, s, p, dp);
-        dp[i][j] = result ? 1 : 0;
-        return result;
-    }
     public boolean isMatch(String s, String p) {
-        int m = s.length(), n = p.length();
-        int[][] dp = new int[m][n];
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
-        return f(m - 1, n - 1, s, p, dp);
+        int m = s.length(), n = p.length(), i = 1, j = 1;
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        
+        dp[0][0] = true;
+        while(j <= n && p.charAt(j - 1) == '*')
+            dp[0][j++] = true;
+        
+        for(i = 1;i <= m;i++)
+            for(j = 1;j <= n;j++)
+                if(s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?')
+                    dp[i][j] = dp[i - 1][j - 1];
+                else if(p.charAt(j - 1) == '*')
+                    dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
+        return dp[m][n];
     }
 }
