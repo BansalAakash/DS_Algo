585136c52e862bc8b328bd13920d3162cadbce55
Aakash Bansal <aakash6025@gmail.com>
2022-11-11 11:48:26 +0530

    Time: 34 ms (63.10%), Space: 43.7 MB (46.76%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,28 +1,26 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int n = nums.length, max = 0, maxIndex = -1;
-        int[] dp = new int[n];
-        int[] parent = new int[n];
         Arrays.sort(nums);
+        int n = nums.length, maxI = 0, max = 1;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int[] parent = new int[n];
         Arrays.fill(parent, -1);
-        for(int i = 0;i < n;i++){
-            for(int j = i - 1;j >= 0;j--)
+        for(int i = 1;i < n;i++)
+            for(int j = 0;j < i;j++)
                 if(nums[i] % nums[j] == 0 && dp[i] <= dp[j]){
-                    dp[i] = dp[j];
+                    dp[i] = dp[j] + 1;
                     parent[i] = j;
+                    if(dp[i] > max){
+                        max = dp[i];
+                        maxI = i;
+                    }
                 }
-            dp[i] += 1;
-            if(dp[i] > max){
-                max = dp[i];
-                maxIndex = i;
-            }
+        LinkedList<Integer> list = new LinkedList<>();
+        while(maxI != -1){
+            list.addFirst(nums[maxI]);
+            maxI = parent[maxI];
         }
-        int i = maxIndex;
-        LinkedList<Integer> result = new LinkedList<>();
-        while(i != -1){
-            result.addFirst(nums[i]);
-            i = parent[i];
-        }
-        return result;
+        return list;
     }
 }
