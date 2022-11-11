261abb667cd93b9405b5ebe5bccc5c00ce2f9a8a
Aakash Bansal <aakash6025@gmail.com>
2022-11-11 07:27:12 +0530

    Time: 8 ms (53.34%), Space: 44.3 MB (30.82%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,25 +1,28 @@
 class Solution {
-    int f(int i, int n, int k, int[] prices, int canBuy, int[][][] dp){
-        if(i == n || k == 0)
-            return 0;
-        if(dp[canBuy][k][i] != -1)
-            return dp[canBuy][k][i];
-        int profit = 0;
-        if(canBuy == 1)
-            profit = Math.max(f(i + 1, n, k, prices, 0, dp) - prices[i], f(i + 1, n, k, prices, 1, dp));
-        else
-            profit = Math.max(f(i + 1, n, k - 1, prices, 1, dp) + prices[i], f(i + 1, n, k, prices, 0, dp));
-        return dp[canBuy][k][i] = profit;
-    }
+    // int f(int i, int n, int k, int[] prices, int canBuy, int[][][] dp){
+    //     if(i == n || k == 0)
+    //         return 0;
+    //     if(dp[canBuy][k][i] != -1)
+    //         return dp[canBuy][k][i];
+    //     int profit = 0;
+    //     if(canBuy == 1)
+    //         profit = Math.max(f(i + 1, n, k, prices, 0, dp) - prices[i], f(i + 1, n, k, prices, 1, dp));
+    //     else
+    //         profit = Math.max(f(i + 1, n, k - 1, prices, 1, dp) + prices[i], f(i + 1, n, k, prices, 0, dp));
+    //     return dp[canBuy][k][i] = profit;
+    // }
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[2][k + 1][n + 1];
-        for(int j = k - 1; j >= 0;j--){
-            for(int i = n - 1;i >= 0;i--){
-                dp[1][j][i] = Math.max(dp[0][j][i + 1] - prices[i], dp[1][j][i + 1]);
-                dp[0][j][i] = Math.max(dp[1][j + 1][i + 1] + prices[i], dp[0][j][i + 1]);
+        k *= 2;
+        int[][] dp = new int[k + 1][n + 1];
+        for(int i = n - 1;i >= 0;i--){
+            for(int j = k - 1; j >= 0;j--){
+                if(j % 2 == 0)
+                    dp[j][i] = Math.max(dp[j + 1][i + 1] - prices[i], dp[j][i + 1]);
+                else
+                    dp[j][i] = Math.max(dp[j + 1][i + 1] + prices[i], dp[j][i + 1]);
             }
         }
-        return dp[1][0][0];
+        return dp[0][0];
     }
 }
