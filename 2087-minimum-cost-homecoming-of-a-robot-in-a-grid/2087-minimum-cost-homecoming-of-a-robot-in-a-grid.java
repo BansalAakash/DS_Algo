d967e2ad9b2c0e9910d55a2593e2e1cf927739d3
Aakash Bansal <aakash6025@gmail.com>
2022-11-15 13:18:32 +0530

    Time: 8 ms (45.69%), Space: 125.5 MB (71.57%) - LeetHub

--- 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.java
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.java
@@ -1,17 +1,10 @@
 class Solution {
     public int minCost(int[] startPos, int[] homePos, int[] rowCosts, int[] colCosts) {
-        int i = startPos[0], j = startPos[1], i_ = homePos[0], j_ = homePos[1];
-        int cost = 0, i_dir = i < i_ ? 1 : -1, j_dir = j < j_ ? 1 : -1;
-        while(i != i_ || j != j_){
-            if(i != i_){
-                i += i_dir;
-                cost += rowCosts[i];
-            }
-            if(j != j_){
-                j += j_dir;
-                cost += colCosts[j];
-            }
-        }
+        int cost = 0, i_dir = startPos[0] < homePos[0] ? 1 : -1, j_dir = startPos[1] < homePos[1] ? 1 : -1;
+        for(int i = startPos[0];i != homePos[0];i += i_dir)
+            cost += rowCosts[i + i_dir];
+        for(int j = startPos[1];j != homePos[1];j += j_dir)
+            cost += colCosts[j + j_dir];
         return cost;
     }
 }
