d49a3bd40a3074e6bd2280cfba0d96ae0549cb4d
Aakash Bansal <aakash6025@gmail.com>
2022-11-19 13:44:53 +0530

    Time: 7 ms (47.30%), Space: 41.4 MB (9.60%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -2,11 +2,8 @@ class Solution {
     int sumDivisors(int n){
         int sum = 1;
         for(int i = 2;i * i <= n;i++)
-            if(n % i == 0){
-                sum += i;
-                if(i != n / i)
-                    sum += (n / i);
-            }
+            if(n % i == 0)
+                sum += i + (n / i);
         return sum;
     }
     public boolean checkPerfectNumber(int num) {
