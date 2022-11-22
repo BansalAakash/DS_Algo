7907a14284e4d617c39da83b05e9018fd3e13c63
Aakash Bansal <aakash6025@gmail.com>
2022-11-22 11:02:49 +0530

    Time: 64 ms (50.01%), Space: 119.4 MB (5.80%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -4,13 +4,12 @@ class Solution {
         int prefixSum = 0, n = nums.length;
         for(int i = 0;i < n;i++) {
         	prefixSum += nums[i];
-                if(i > 0 && prefixSum % k == 0)
-                    return true;
-                for(int j = 0;j * k <= prefixSum;j++)
-                    if(map.containsKey(prefixSum - j * k) && i - map.get(prefixSum - j * k) > 1)
-                        return true;
-            if(!map.containsKey(prefixSum))
-        	    map.put(prefixSum, i);
+            if(i > 0 && prefixSum % k == 0)
+                return true;
+            if(map.containsKey(prefixSum % k) && i > map.get(prefixSum % k) + 1)
+                return true;
+            if(!map.containsKey(prefixSum % k))
+        	    map.put(prefixSum % k, i);
         }
         return false;
     }
