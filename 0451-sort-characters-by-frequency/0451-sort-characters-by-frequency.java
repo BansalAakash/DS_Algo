98d8288cec6c3ace33ed6c7e0a88b174df1d2629
Aakash Bansal <aakash6025@gmail.com>
2022-11-24 13:22:59 +0530

    Time: 36 ms (57.81%), Space: 49.3 MB (52.01%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,33 +1,19 @@
 //digits...upper...lower
 class Solution {
     public String frequencySort(String s) {
-        int[] freq = new int[62];
-        int maxFreq = -1;
-        for(char ch : s.toCharArray()){
-            int i = 0;
-            if(ch >= 'a')
-                i = ch - 'a' + 36;
-            else if(ch >= 'A')
-                i = ch - 'A' + 10;
-            else
-                i = ch - '0';
-            freq[i]++;
-        }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        for(int i = 0;i < 62;i++)
-            pq.add(new int[] {i, freq[i]});
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray())
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        pq.addAll(map.entrySet());
         StringBuilder str = new StringBuilder();
         while(!pq.isEmpty()){
-            int[] cur = pq.poll();
-            int ch = 0;
-            if(cur[0] < 10)
-                ch = cur[0] + '0';
-            else if(cur[0] < 36)
-                ch = cur[0] - 10 + 'A';
-            else ch = cur[0] - 36 + 'a';
-            int count = cur[1];
+            Map.Entry<Character, Integer> cur = pq.poll();
+            int count = cur.getValue();
+            char ch = cur.getKey();
             while(count-- > 0)
-                str.append((char)ch);
+                str.append(ch);
         }
         return str.toString();
     }
