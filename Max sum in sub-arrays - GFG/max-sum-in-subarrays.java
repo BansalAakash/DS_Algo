405a7bec67742f45d279be784b686770195036ca
Aakash Bansal <aakash6025@gmail.com>
2022-11-25 11:22:37 +0530

    Added solution - LeetHub

---
+++ Max sum in sub-arrays - GFG/max-sum-in-subarrays.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.pairWithMaxSum(a, n));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public static long pairWithMaxSum(long arr[], long N)
+    {
+        long cur = arr[0] + arr[1], max = cur;
+        for(int i = 2;i < N;i++){
+            cur = cur - arr[i - 2] + arr[i];
+            max = Math.max(cur, max);
+        }
+        return max;
+    }
+}
