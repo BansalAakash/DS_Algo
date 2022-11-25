1564e2bf7da155f1e6887196a38ad29b30724fea
Aakash Bansal <aakash6025@gmail.com>
2022-11-25 12:01:38 +0530

    Time: 62 ms (70.53%), Space: 150.6 MB (45.50%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,23 +1,17 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         ArrayDeque<Integer> dq = new ArrayDeque<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
         int n = nums.length, index = 0;
         int[] result = new int[n - k + 1];
         
         for(int i = 0;i < n;i++){
-            if(i >= k && map.get(dq.getFirst()) == i - k){
-                map.remove(dq.getFirst());
+            if(i >= k && dq.getFirst() == i - k)
                 dq.removeFirst();
-            }
-            while(!dq.isEmpty() && dq.getLast() <= nums[i]){
-                map.remove(dq.getLast());
+            while(!dq.isEmpty() && nums[dq.getLast()] <= nums[i])
                 dq.removeLast();
-            }
-            dq.addLast(nums[i]);
-            map.put(nums[i], i);
+            dq.addLast(i);
             if(i >= k - 1)
-                result[index++] = dq.getFirst();
+                result[index++] = nums[dq.getFirst()];
         }
         return result;
     }
