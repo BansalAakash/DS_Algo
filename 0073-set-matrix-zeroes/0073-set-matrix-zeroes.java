e298776ee6f0c0376b2517674db96e0974767f0c
Aakash Bansal <aakash6025@gmail.com>
2022-11-27 19:28:56 +0530

    Time: 1 ms (97.18%), Space: 54.4 MB (26.69%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,17 +1,38 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        boolean[] rows = new boolean[m];
-        boolean[] cols = new boolean[n];
+    public void setZeroes(int[][] mat) {
+        boolean firstColZero = false, firstRowZero = false;
+        int m = mat.length, n = mat[0].length;
+        
         for(int i = 0;i < m;i++)
-            for(int j = 0;j < n;j++)
-                if(matrix[i][j] == 0){
-                    rows[i] = true;
-                    cols[j] = true;
+            if(mat[i][0] == 0){
+                firstColZero = true;
+                break;
+            }
+        
+        for(int j = 0;j < n;j++)
+            if(mat[0][j] == 0){
+                firstRowZero = true;
+                break;
+            }
+        
+        for(int i = 1;i < m;i++)
+            for(int j = 1;j < n;j++)
+                if(mat[i][j] == 0){
+                    mat[0][j] = 0;
+                    mat[i][0] = 0;
                 }
-        for(int i = 0;i < m;i++)
+        
+        for(int i = 1;i < m;i++)
+            for(int j = 1;j < n;j++)
+                if(mat[i][0] == 0 || mat[0][j] == 0)
+                    mat[i][j] = 0;
+        
+        if(firstColZero)
+            for(int i = 0;i < m;i++)
+                mat[i][0] = 0;
+        if(firstRowZero)
             for(int j = 0;j < n;j++)
-                if(rows[i] || cols[j])
-                    matrix[i][j] = 0;
+                mat[0][j] = 0;
+        
     }
 }
