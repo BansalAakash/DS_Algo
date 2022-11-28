d404941c9a7dbd54b8cb96e9dc99a278c73e6c7a
Aakash Bansal <aakash6025@gmail.com>
2022-11-28 10:00:33 +0530

    Time: 1 ms (97.09%), Space: 54.7 MB (7.65%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -9,13 +9,7 @@ class Solution {
                 break;
             }
         
-        for(int j = 0;j < n;j++)
-            if(mat[0][j] == 0){
-                firstRowZero = true;
-                break;
-            }
-        
-        for(int i = 1;i < m;i++)
+        for(int i = 0;i < m;i++)
             for(int j = 1;j < n;j++)
                 if(mat[i][j] == 0){
                     mat[0][j] = 0;
@@ -27,12 +21,14 @@ class Solution {
                 if(mat[i][0] == 0 || mat[0][j] == 0)
                     mat[i][j] = 0;
         
+        if(mat[0][0] == 0)
+            for(int j = 0;j < n;j++)
+                mat[0][j] = 0;
+        
         if(firstColZero)
             for(int i = 0;i < m;i++)
                 mat[i][0] = 0;
-        if(firstRowZero)
-            for(int j = 0;j < n;j++)
-                mat[0][j] = 0;
+        
         
     }
 }
