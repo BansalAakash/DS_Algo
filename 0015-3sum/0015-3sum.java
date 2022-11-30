a1b5685f03c5b88b9afdd30740824370bc5b9899
Aakash Bansal <aakash6025@gmail.com>
2022-11-30 11:54:03 +0530

    Time: 30 ms (81.68%), Space: 46.6 MB (90.09%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,28 +1,44 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        int n = nums.length, i = 0;
+        return kSum(nums, 0, 0, 3);
+    }
+    private List<List<Integer>> kSum(int[] nums, int start, long target, int k){
+        int n = nums.length;
+        long avg = target / k;
         List<List<Integer>> result = new ArrayList<>();
-        while(i < n - 2){
-            int j = i + 1, k = n - 1;
-            while(j < k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(sum == 0){
-                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    do{
-                        j++;
-                    } while(j < k && nums[j] == nums[j - 1]);
-                    do{
-                        k--;
-                    } while(j < k && nums[k] == nums[k + 1]);
-                }
-                else if(sum < 0)
-                    j++;
-                else k--;
+        if(start > n - k || nums[start] > avg || nums[n - 1] < avg)
+            return result;
+        if(k == 2)
+            return twoSum(nums, start, target);
+        for(int i = start;i < n;i++){
+            if(i > start && nums[i] == nums[i - 1])
+                continue;
+            for(List<Integer> list : kSum(nums, i + 1, target - nums[i], k - 1)){
+                List<Integer> cur = new ArrayList<>();
+                cur.add(nums[i]);
+                cur.addAll(list);
+                result.add(cur);
             }
-            do{
-                i++;
-            } while(i < n && nums[i] == nums[i - 1]);
+        }
+        return result;
+    }
+    private List<List<Integer>> twoSum(int[] nums, int start, long target){
+        int i = start, j = nums.length - 1;
+        List<List<Integer>> result = new ArrayList<>();
+        while(i < j){
+            int sum = nums[i] + nums[j];
+            if(sum == target){
+                result.add(Arrays.asList(nums[i], nums[j]));
+                do{
+                    i++;
+                } while(i < j && nums[i] == nums[i - 1]);
+                do{
+                    j--;
+                } while(i < j && nums[j] == nums[j + 1]);
+            } else if(sum > target)
+                j--;
+            else i++;
         }
         return result;
     }
