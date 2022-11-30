99a7f73f73ecb96f16ec4711640a413a5683ab7d
Aakash Bansal <aakash6025@gmail.com>
2022-11-30 11:51:51 +0530

    Time: 4 ms (99.56%), Space: 42.5 MB (97.01%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,38 +1,44 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        int i = 0, n = nums.length;
+        return kSum(nums, 0, target, 4);
+    }
+    private List<List<Integer>> kSum(int[] nums, int start, long target, int k){
+        int n = nums.length;
+        long avg = target / k;
         List<List<Integer>> result = new ArrayList<>();
-        if(nums[n - 1] < target / 4)
+        if(start > n - k || nums[start] > avg || nums[n - 1] < avg)
             return result;
-        while(i < n - 3){
-            if(nums[i] > target / 4)
-                return result;
-            
-            int j = i + 1;
-            while(j < n - 2){
-                int k = j + 1, l = n - 1;
-                while(k < l){
-                    int sum = nums[i] + nums[j] + nums[k] + nums[l];
-                    if(sum == target){
-                        result.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
-                        do{
-                            k++;
-                        } while(k < l && nums[k] == nums[k - 1]);
-                        do{
-                            l--;
-                        } while(k < l && nums[l] == nums[l + 1]);
-                    } else if(sum < target){
-                        k++;
-                    } else l--;
-                }
-                do{
-                    j++;
-                } while(j < n - 2 && nums[j] == nums[j - 1]);
+        if(k == 2)
+            return twoSum(nums, start, target);
+        for(int i = start;i < n;i++){
+            if(i > start && nums[i] == nums[i - 1])
+                continue;
+            for(List<Integer> list : kSum(nums, i + 1, target - nums[i], k - 1)){
+                List<Integer> cur = new ArrayList<>();
+                cur.add(nums[i]);
+                cur.addAll(list);
+                result.add(cur);
             }
-            do{
+        }
+        return result;
+    }
+    private List<List<Integer>> twoSum(int[] nums, int start, long target){
+        int i = start, j = nums.length - 1;
+        List<List<Integer>> result = new ArrayList<>();
+        while(i < j){
+            int sum = nums[i] + nums[j];
+            if(sum == target){
+                result.add(Arrays.asList(nums[i], nums[j]));
+                do{
                     i++;
-                } while(i < n - 3 && nums[i] == nums[i - 1]);
+                } while(i < j && nums[i] == nums[i - 1]);
+                do{
+                    j--;
+                } while(i < j && nums[j] == nums[j + 1]);
+            } else if(sum > target)
+                j--;
+            else i++;
         }
         return result;
     }
