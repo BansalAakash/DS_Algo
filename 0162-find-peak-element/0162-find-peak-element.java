8010147cd8b8a20357ab9aa1a28d9cec3d8d882b
Aakash Bansal <aakash6025@gmail.com>
2022-12-04 16:04:08 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (75.93%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,26 +1,11 @@
 class Solution {
-    boolean isPeak(int[] nums, int i, int n){
-        if(n == 1)
-            return true;
-        if(i == n - 1){
-            if(nums[i] > nums[i - 1])
-                return true;
-            else return false;
-        }
-        if(i == 0){
-            if(nums[i] > nums[i + 1])
-                return true;
-            else return false;
-        }
-        return nums[i] > Math.max(nums[i - 1], nums[i + 1]);
-    }
     int binarySearch(int[] nums, int low, int high, int n){
-        if(low > high)
-            return -1;
+        if(low == high)
+            return low;
         int mid = low + (high - low) / 2;
-        if(isPeak(nums, mid, n))
-            return mid;
-        return Math.max(binarySearch(nums, low, mid - 1, n), binarySearch(nums, mid + 1, high, n));
+        if(nums[mid] > (mid == n - 1 ? Integer.MIN_VALUE : nums[mid + 1]))
+            return binarySearch(nums, low, mid, n);
+        return binarySearch(nums, mid + 1, high, n);
     }
     public int findPeakElement(int[] nums) {
         int n = nums.length;
